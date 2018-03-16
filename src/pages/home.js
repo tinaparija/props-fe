a98f82724bb06abc35ad8a2fb5071834bd5b76c7
tinaparija <tinaparija@gmail.com>
2018-03-16 14:32:15 +0530

    gitignore

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,34 @@ class Home extends Component {
   render() {
     return (
       <div>
-      Hi
+        <div className ="row top-bar">
+          <div className ="col col-md-1">
+            <a href="https://www.w3schools.com">Home</a>
+          </div>
+          <div className ="col col-md-1">
+            <a href="https://www.w3schools.com">List</a>
+          </div>
+        </div>
+        <div className ="row bottom-bar">
+          <div className = "col col-md-6 offset-md-3">
+            <div className = "category learn">
+            <p className="cat-name">Learn</p>
+            </div>
+            <div className = "category create">
+            <p className="cat-name">Create</p>
+            </div>
+          </div>
+        </div>
+        <div className ="row">
+          <div className = "col col-md-6 offset-md-3">
+            <div className = "category love">
+            <p className="cat-name">Love</p>
+            </div>
+            <div className = "category rest">
+            <p className="cat-name">Rest</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
